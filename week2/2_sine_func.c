935b8058aad9a66153c34dfb5e0e94899195c3f2
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-08-08 16:13:34 +0800

    Optim

--- week2/2_sine_func.c
+++ week2/2_sine_func.c
@@ -1,12 +1,16 @@
+// this program takes an input x and compute the sin(x)
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
 
 int main(void)
 {
-    double x;
+    double x = 0.0;
+    // Read x from input
+    printf("Input a decimal point number between 0 and 1: ");
     scanf("%lf", &x);
     //printf("x: %lf, sin(s): %lf \n", x, sin(x));
-    printf("%lf", sin(x));
+    // Calculate the sine of given x
+    printf("The sine of %lf is : %lf\n", x, sin(x));
     return 0;
 }
